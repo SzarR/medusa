e4738794acc75958759c9ec9e54ee7a350abc815
Frank Mu <41923525+fxqmu@users.noreply.github.com>
2021-06-06 21:12:41 -0400

    Update to use lubridate and set NA ranges
    Use lubridate functions to get a slightly more accurate duration between birth date and current date. Also lubridate is part of expanded tidyverse so more pipe friendly, so changing this function to use piping as well. In terms of NA ranges for age, some birthdate data is clearly wrong, so changing code to first set NA for birth dates that are clearly wrong - those who indicate that they are under 18 or over 100 years of age based on birth year.

--- R/step_agegroup.R
+++ R/step_agegroup.R
@@ -2,6 +2,8 @@
 #' to best practices. Age is calculated from the date the function was ran.
 #' Results may therefore vary.
 #'
+#' @importFrom lubridate
+#'
 #' @param df a tibble of demographic data
 #'
 #' @return Age, AgeGroup
@@ -10,51 +12,43 @@
 #' @examples #step_agegroup(df = demo_raw)
 step_agegroup <- function(df) {
 
-  df$Birthdate <-
-    as.Date(df$Birthdate,
-            format= "%m/%d/%Y")
-
-  df$todaysdate <-
-    as.Date(format(Sys.Date(), "%m/%d/%Y"), "%m/%d/%Y")
-
-  df$Age <-
-    as.numeric(difftime(df$todaysdate, df$Birthdate, units = "weeks")) / 52.25
-
-  df$Age <-
-    round(df$Age, digits = 0)
-
-  df$AgeGroup <-
-    cut(df$Age,
-        breaks =
-          c(
-            -Inf,
-            20,
-            24,
-            29,
-            34,
-            39,
-            44,
-            49,
-            54,
-            59,
-            64,
-            69,
-            Inf
-          ),
-        labels =
-          c(
-            "20 and under",
-            "20-24",
-            "25-29",
-            "30-34",
-            "35-39",
-            "40-44",
-            "45-49",
-            "50-54",
-            "55-59",
-            "60-64",
-            "65-69",
-            "70+")
+  df <- df %>%
+    mutate(Birthdate = as.Date(Birthdate, format = "%m/%d/%Y")) %>%
+    mutate(Age = lubridate::time_length(
+      lubridate::interval(start = Birthdate, end = Sys.Date()), unit = "year")) %>%
+    mutate(Age = round(Age, digits = 0)) %>%
+    mutate(Age = case_when(Age < 18 | Age > 100 ~ NA_real_, 
+                           Age >= 18 & Age <= 100 ~ Age)) %>% 
+    mutate(AgeGroup = cut(
+      Age,
+      breaks =
+        c(-Inf,
+          20,
+          24,
+          29,
+          34,
+          39,
+          44,
+          49,
+          54,
+          59,
+          64,
+          69,
+          Inf),
+      labels =
+        c("20 and under",
+          "20-24",
+          "25-29",
+          "30-34",
+          "35-39",
+          "40-44",
+          "45-49",
+          "50-54",
+          "55-59",
+          "60-64",
+          "65-69",
+          "70+")
+      )
     )
   return(df)
 }
