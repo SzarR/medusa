2c808b37f3ea81d0b2fb3cb6aa2c731c7cfe5b92
Frank Mu <41923525+fxqmu@users.noreply.github.com>
2023-08-28 14:52:44 -0400

    Update make_final_data to be more flexible for unnested columns
    Fixed final selection for `org_aff_[x]` and `inv_[x]` columns to make them more flexible, since they're both unnested from values in a single column.

--- R/make_final_data.R
+++ R/make_final_data.R
@@ -61,25 +61,9 @@ make_final_data <- function(demo, dues, staff) {
       .data$int_1,
       .data$int_2,
       .data$int_3,
-      .data$org_aff_AOM,
-      .data$org_aff_APA,
-      .data$org_aff_APS,
-      .data$org_aff_SHRM,
-      .data$org_aff_EAWOP,
-      .data$org_aff_IAAP,
-      .data$org_aff_CPA,
-      .data$org_aff_HRCI,
-      .data$org_aff_SHRM,
+      starts_with("org_aff_"),
       .data$licensed,
-      .data$inv_share_knowledge,
-      .data$inv_collaborate_on_projects,
-      .data$inv_conduct_research,
-      .data$inv_join_interdisciplinary_teams,
-      .data$inv_other,
-      .data$inv_present_at_meetings_conferences,
-      .data$inv_join_communities_of_interest,
-      .data$inv_network,
-      .data$inv_consult_to_business
+      starts_with("inv_")
 
 
     ) %>%
